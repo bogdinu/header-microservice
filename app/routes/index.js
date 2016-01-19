f62ac0a1cbad11b2e893cba5169f3a74023068b4
bogdinu <bogdinu@yahoo.fr>
2016-01-19 22:18:39 +0000

    Timestamp application

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,24 @@
 'use strict';
 
-var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+var moment = require("moment")
 
-module.exports = function (app, passport) {
+module.exports = function (app) {
+	app.route('/:timeStamp').get(function(req, res){
+		//app.get('/', function(req, res){ --> alternate sytax : what's the difference ?
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
+		var objOut = {};
+		/*if (isNaN(req.params.timeStamp)){ //if is not a number, it should be a natural language form
+			objOut.natural = req.params.timeStamp;
 		}
-	}
-
-	var clickHandler = new ClickHandler();
-
-	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
-
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
+		else { //unix format
+			objOut.unix = req.params.timeStamp;
+			var unixDate = moment.unix(req.params.timeStamp);
+			objOut.natural = moment(unixDate).format('MMMM D YYYY');
+		}*/
+		
+		objOut.unix = req.params.timeStamp;
+		objOut.natural = moment(req.params.timeStamp).format('MMMM D YYYY');
+		
+		res.send(JSON.stringify(objOut));
+	});
 };
